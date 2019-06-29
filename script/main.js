8c19f851a6548b60f6b92f83ad9b6a35a1538cb6
Maria Yudintseva <yudintseva2708@gmail.com>
2019-06-29 11:49:30 +0300

    Lesson 02, add let budgetDay, console.log

--- script/main.js
+++ script/main.js
@@ -1,5 +1,24 @@
 alert('Hello World');
 let money, income, addExpenses, deposit, mission, period;
-money = 10;
-console.log(money);
+money = 350000;
+income = 'freelance';
+addExpenses = 'Food, Technics, Housing';
+deposit = true;
+mission = 1000000;
+period = 11;
 
+
+
+console.log('money - ', typeof money);
+console.log('deposit - ', typeof deposit);
+console.log('income - ', typeof income);
+console.log('Income length', income.length);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+console.log('Период: ', period, 'месяцев');
+console.log('Цель заработать: ', mission, 'гривен');
+
+let budgetDay = money / 30;
+let percent = money % 30;
+console.log('Дневной бюджет', budgetDay,'Oстаток от деления', percent);
