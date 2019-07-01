cdb80b0f1ac823701b00f46d48cb91184fffc8be
Maria Yudintseva <yudintseva2708@gmail.com>
2019-07-01 20:47:31 +0300

    lesson03-main, if/else

--- script/main.js
+++ script/main.js
@@ -1,24 +1,53 @@
-alert('Hello World');
+'use strict';
+
 let money, income, addExpenses, deposit, mission, period;
-money = 350000;
-income = 'freelance';
-addExpenses = 'Food, Technics, Housing';
-deposit = true;
+money = prompt('Ваш месячный доход');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+addExpenses = addExpenses.split(', ');
+console.log(addExpenses);
+deposit = prompt('Есть ли у вас депозит в банке');
+deposit = Boolean(deposit);
 mission = 1000000;
-period = 11;
 
-
-
+if (deposit='Нет'){
+    deposit = false;
+}
+console.log('deposit - ', deposit);
 console.log('money - ', typeof money);
-console.log('deposit - ', typeof deposit);
 console.log('income - ', typeof income);
-console.log('Income length', income.length);
-console.log(addExpenses.toLowerCase());
-console.log(addExpenses.split(', '));
+console.log('deposit - ', typeof deposit);
 
-console.log('Период: ', period, 'месяцев');
-console.log('Цель заработать: ', mission, 'гривен');
+let question1 = prompt('Какие обязательные ежемесячные расходы у вас есть? (одно слово)');
+let question2 = prompt('Какие обязательные ежемесячные расходы у вас есть? (одно слово)');
+let question3 = +prompt('Во сколько это обойдется?', question1);
+let question4 = +prompt('Во сколько это обойдется?', question2);
 
-let budgetDay = money / 30;
-let percent = money % 30;
-console.log('Дневной бюджет', budgetDay,'Oстаток от деления', percent);
+
+let budgetMonth = money - question3 - question4;
+console.log('budget month = ', budgetMonth);
+
+let month = mission / money;
+console.log('Mission month', Math.ceil(month));
+
+// console.log('Income length', income.length);
+// console.log(addExpenses.toLowerCase());
+// console.log(addExpenses.split(', '));
+
+// console.log('Период: ', period, 'месяцев');
+// console.log('Цель заработать: ', mission, 'гривен');
+
+let budgetDay = budgetMonth / 30;
+console.log('budget day: ', Math.floor(budgetDay))
+
+if (budgetDay=>800){
+    document.write('Высокий уровень дохода');
+}
+else if((budgetDay>=300) && (budgetDay<=800)){
+    document.write('Средний уровень дохода');
+}
+else if((budgetDay=>0) && (budgetDay<=300)){
+    document.write('Низкий уровень дохода');
+}
+else{
+    document.write('Что то пошло не так');
+}
