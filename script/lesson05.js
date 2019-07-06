20aeb884285f2d5c264423ea4416d80635d6405c
Maria Yudintseva <yudintseva2708@gmail.com>
2019-07-06 12:50:44 +0300

    Correct

--- script/lesson05.js
+++ script/lesson05.js
@@ -21,3 +21,21 @@ let array = ['2134', '5234523', '42324', '432', '7875', '34896', '27564'];
 array.forEach(em => {
   if (String(em).search(/(2|4)/) === 0) console.log(em);
 })
+
+let func = function(num1, num2){
+  const sumArr = [];
+  for(let i = num1; i <= num2; i++){
+    let sum = 0;
+  
+  for(let j = 0; j <= i; j++){
+    if(i % j === 0){ 
+      sum += j;
+      continue;
+    }
+  }
+  sumArr.push(sum);
+  console.log(sum, 'Делители этого числа 1 и ', i)
+}
+return sumArr;
+}
+console.log(func(1, 100))
