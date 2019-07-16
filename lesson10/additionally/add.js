ab6ef1d4da03d7f61362aca5be45fe80ecac34d5
Maria Yudintseva <yudintseva2708@gmail.com>
2019-07-16 20:40:30 +0300

    Lesson10 this

--- lesson10/additionally/add.js
+++ lesson10/additionally/add.js
@@ -3,37 +3,37 @@ document.addEventListener('DOMContentLoaded', function(){
     let body = document.querySelector('body');
     let division = document.createElement('div');
 
-    const DomElelement = {
-        selector: '',
-        height: 100 + 'px',
-        width: 100 + 'px',
-        background: '#ccc',
-        fontsize: 20 + 'px',
-        position: 'absolute',
-        left: 0,
-        top: 0
+    const DomElelement = function(selector, height, width, background, fontsize, position, left, top){
+        this.selector = selector; //'',
+        this.height =  height;//100 + 'px',
+        this.width =  width;//100 + 'px',
+        this.background = background; //'#ccc',
+        this.fontsize =  fontsize;//20 + 'px',
+        this.position =  position;//'absolute',
+        this.left =  left;//0,
+        this.top = top //0
     };
-
     body.appendChild(division);
-    division.style.cssText = "background: " + DomElelement.background + ';' + "width: " + DomElelement.width + ';' + "height:" + DomElelement.height+ ';' + "font-size:" + DomElelement.fontsize + ';' + "position: " + DomElelement.position;
+    let domElelement = new DomElelement('', 100 + 'px', 100 + 'px', '#ccc', 20 + 'px', 'absolute', 0, 0)
+    division.style.cssText = "background: " + domElelement.background + ';' + "width: " + domElelement.width + ';' + "height:" + domElelement.height+ ';' + "font-size:" + domElelement.fontsize + ';' + "position: " + domElelement.position;
     function keys(e){
         event = window.event ? window.event : e;
         switch (e.keyCode) {
             case 37:
-                DomElelement.left = DomElelement.left - 10;
-                division.style.left = DomElelement.left + 'px';
+                domElelement.left = domElelement.left - 10;
+                division.style.left = domElelement.left + 'px';
                 break; 
             case 38:
-                DomElelement.top = DomElelement.top + 10;
-                division.style.top = DomElelement.top + 'px';
+                domElelement.top = domElelement.top + 10;
+                division.style.top = domElelement.top + 'px';
                 break;
             case 39:
-                DomElelement.left = DomElelement.left + 10;
-                division.style.left = DomElelement.left + 'px';
+                domElelement.left = domElelement.left + 10;
+                division.style.left = domElelement.left + 'px';
                 break;
             case 40:
-                DomElelement.top = DomElelement.top - 10;
-                division.style.top = DomElelement.top + 'px';
+                domElelement.top = domElelement.top - 10;
+                division.style.top = domElelement.top + 'px';
                 break;
         }
     }
--- lesson10/main.js
+++ lesson10/main.js
@@ -4,28 +4,28 @@ let body = document.querySelector('body');
 let division = document.createElement('div');
 let paragraph = document.createElement('p');
 
-const DomElelement = {
-    selector: '',
-    height: 100 + 'px',
-    width: 100 + 'px',
-    background: '#ccc',
-    fontsize: 20 + 'px',
+const DomElelement = function(selector, height, width, background, fontsize){
+    this.selector = selector; //'',
+    this.height =  height;//100 + 'px',
+    this.width = width; //100 + 'px',
+    this.background = background; //'#ccc',
+    this.fontsize =  fontsize;//20 + 'px',
 };
 
 let create = function(){
-    let method = Object.create(DomElelement);
+    let method = new DomElelement('', 100 + 'px', 100 + 'px', '#ccc', 20 +'px');
     method.selector = '#p';
     let select = method.selector;
 
 for(let i = 0; i < select.length; i++){
     if(select[0] == '.'){
         body.appendChild(division);
-        division.style.cssText = "background: " + method.__proto__.background + ';' + "width: " + method.__proto__.width + ';' + "height:" + method.__proto__.height+ ';' + "font-size:" + method.__proto__.fontsize;
+        division.style.cssText = "background: " + method.background + ';' + "width: " + method.width + ';' + "height:" + method.height+ ';' + "font-size:" + method.fontsize;
         division.textContent = 'Division - Hello World!';
     }
     if(select[0] == '#'){
         body.appendChild(paragraph);
-        paragraph.style.cssText = "background: " + method.__proto__.background + ';' + "width: " + method.__proto__.width + ';' + "height:" + method.__proto__.height+ ';' + "font-size:" + method.__proto__.fontsize;
+        paragraph.style.cssText = "background: " + method.background + ';' + "width: " + method.width + ';' + "height:" + method.height+ ';' + "font-size:" + method.fontsize;
         paragraph.innerHTML = 'Paragraph - Hello World!';
     }
 }
